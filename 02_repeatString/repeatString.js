e07f9f81e2f464c628445588fbec52c578aa01bf
Tatiana <110640811+Tatianaharvey@users.noreply.github.com>
2022-09-04 16:00:34 -0700

    fix 1st bug

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    let result = '';
+    let i = 0; 
+    while (i < num) {
+        result += string;
+        i ++
+    }
+  return result 
 };
+
+repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
