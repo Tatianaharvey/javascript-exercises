a7205c77d17c6286f3553fe3f514e2dc48bf9594
Tatiana <110640811+Tatianaharvey@users.noreply.github.com>
2022-09-05 19:32:04 -0700

    pass test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+   if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) { 
+    return 'ERROR'; 
+}
+   let total = 0
+   if (a < b) {
+      for (let i = a; i <= b; i++) {
+         total += i;
+   }
+   } else (a > b); {
+        for (let i = a; i >=b; i--) {
+            total += i;
+        }
+   }
 
+   return total;
+   
 };
 
 // Do not edit below this line
